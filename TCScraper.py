4552101260b73a339add4483b413abb4089eab50
Agustin Guarino <agustinignacio.guarinobonahon@ukg.com>
2023-10-23 15:56:56 -0300

    Added initial structure for failure scraping

--- TCScraper.py
+++ TCScraper.py
@@ -1,11 +1,13 @@
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from time import sleep
-import Util as util
+import Util as Util
 
 urls = ["http://teamcity.dev.us.corp/buildConfiguration/UltiPro_V12_4Integration_1Domains_Gate0_00RunTests/52463296?buildTab=tests&status=failed"]
 driver = webdriver.Chrome()
 FirstRun = True
+failure_elements = []
+failure_links = []
 
 for url in urls:
     driver.get(url)
@@ -13,9 +15,25 @@ for url in urls:
     #############################
     if FirstRun:
         FirstRun = False
-        print("Sleeping for 30 secs")
-        sleep(30)
+        print("Sleeping for 50 secs")
+        sleep(50)
+        driver.get(url)
+        print("After sleeping")
     #############################
 
-    failure_elements = driver.find_elements(By.XPATH, "")
+    print("Starting scraping")
+    sleep(5)
+    failure_elements = driver.find_elements(By.XPATH, Util.Failure_Elements_XPATH)
+    print(f"Errors detected: {str(len(failure_elements))}")
+    print("Ending scraping")
+
+print("Starting printing")
+total = 0
+for i in range(0, len(failure_elements)):
+    if i % 4 == 0:
+        failure_links.append(failure_elements[i].get_attribute("href"))
+        print("URL: " , failure_elements[i].get_attribute("href"))
+        total += 1
+
+print(f"Ending printing ({str(total)})")
     
