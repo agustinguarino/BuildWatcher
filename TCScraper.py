75406310040bf331ff231760dd02613042950ce0
Agustin Guarino <agustinignacio.guarinobonahon@ukg.com>
2023-10-23 17:15:02 -0300

    Added navigate to url and reduced sleep for arrow click

--- TCScraper.py
+++ TCScraper.py
@@ -9,15 +9,18 @@ driver = webdriver.Chrome()
 failure_elements = []
 
 for url in urls:
+    driver.get(url)
+
     print("Sleeping for 45 secs")
     sleep(45)
     driver.get(url)
     print("After sleeping")
 
+    driver.get(url)
+
     print("Starting scraping")
     sleep(5)
     failure_elements = driver.find_elements(By.XPATH, Util.Failure_Elements_XPATH)
-    arrow_elements = driver.find_elements(By.XPATH, Util.Expand_Failure_Arrow_XPATH)
     print(f"Errors detected: {str(len(failure_elements))}")
     print("Ending scraping")
 
@@ -26,7 +29,7 @@ for i in range (0, len(failure_elements)):
     print("Arrive")
     arrow_down = driver.find_element(By.XPATH, f"(//div[@class='Details__heading--id TestItem__heading--Xx TestItem__expandable--KK']/span[@class='ring-icon-icon SvgIcon__icon--wZ TestItem__arrow--TC'])[{str(i + 1)}]")
     driver.execute_script("arguments[0].click();", arrow_down)
-    sleep(10)
+    sleep(1)
     test_name = driver.find_element(By.XPATH, f"(//a[@data-test-build-test-name-part='name'])[{str(i + 1)}]").text
     package = driver.find_element(By.XPATH, f"(//a[@data-test-build-test-name-part='package'])[{str(i + 1)}]").text
     stacktrace = driver.find_element(By.XPATH, f"//div[@class='BuildLogMessages__messages--MP']").text
