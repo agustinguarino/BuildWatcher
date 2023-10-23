ea82a3d75caaf97a743f34c25d59f1a0f843a531
Agustin Guarino <agustinignacio.guarinobonahon@ukg.com>
2023-10-23 17:03:16 -0300

    Extracting individual variables

--- TCScraper.py
+++ TCScraper.py
@@ -3,11 +3,12 @@ from selenium.webdriver.common.by import By
 from time import sleep
 import Util as Util
 
-urls = ["http://teamcity.dev.us.corp/buildConfiguration/UltiPro_V12_4Integration_1Domains_Gate0_00RunTests/52463296?buildTab=tests&status=failed"]
+#urls = ["http://teamcity.dev.us.corp/buildConfiguration/UltiPro_V12_4Integration_1Domains_Gate0_00RunTests/52463296?buildTab=tests&status=failed"]
+urls = ["http://teamcity.dev.us.corp/buildConfiguration/UltiPro_V12_4Integration_1Domains_Ues_00RunTests/52453001?buildTab=tests&status=failed"]
 driver = webdriver.Chrome()
 FirstRun = True
 failure_elements = []
-failure_links = []
+arrow_elements = []
 
 for url in urls:
     driver.get(url)
@@ -15,8 +16,8 @@ for url in urls:
     #############################
     if FirstRun:
         FirstRun = False
-        print("Sleeping for 50 secs")
-        sleep(50)
+        print("Sleeping for 45 secs")
+        sleep(45)
         driver.get(url)
         print("After sleeping")
     #############################
@@ -24,16 +25,19 @@ for url in urls:
     print("Starting scraping")
     sleep(5)
     failure_elements = driver.find_elements(By.XPATH, Util.Failure_Elements_XPATH)
+    arrow_elements = driver.find_elements(By.XPATH, Util.Expand_Failure_Arrow_XPATH)
     print(f"Errors detected: {str(len(failure_elements))}")
     print("Ending scraping")
 
-print("Starting printing")
-total = 0
-for i in range(0, len(failure_elements)):
-    if i % 4 == 0:
-        failure_links.append(failure_elements[i].get_attribute("href"))
-        print("URL: " , failure_elements[i].get_attribute("href"))
-        total += 1
 
-print(f"Ending printing ({str(total)})")
+for i in range (0, len(failure_elements)):
+    print("Arrive")
+    arrow_down = driver.find_element(By.XPATH, f"(//div[@class='Details__heading--id TestItem__heading--Xx TestItem__expandable--KK']/span[@class='ring-icon-icon SvgIcon__icon--wZ TestItem__arrow--TC'])[{str(i + 1)}]")
+    driver.execute_script("arguments[0].click();", arrow_down)
+    sleep(10)
+    test_name = driver.find_element(By.XPATH, f"(//a[@data-test-build-test-name-part='name'])[{str(i + 1)}]").text
+    package = driver.find_element(By.XPATH, f"(//a[@data-test-build-test-name-part='package'])[{str(i + 1)}]").text
+    stacktrace = driver.find_element(By.XPATH, f"//div[@class='BuildLogMessages__messages--MP']").text
+    print(f"{test_name} // {package} // {stacktrace}")
+    sleep(0.5)
     
