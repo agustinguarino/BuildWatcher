f8bc55d861d41e4c610c8c2f210e953d6d72c904
Agustin Guarino <agustinignacio.guarinobonahon@ukg.com>
2023-11-06 14:19:03 -0300

    Get build date and include inreturned dict, added escape hotkey before clicking tests

--- BuildConsoleScraper.py
+++ BuildConsoleScraper.py
@@ -8,11 +8,11 @@ from time import sleep
 import Util as Util
 
 driver = webdriver.Chrome()
-url = "https://buildconsole.ulti.io/dashboard/64e62b8a1aa339d31db3edf1/builds?page=0&pagesize=20"
 
 urls = {}
 
 def getBuildConsoleUrls():
+    url = "https://buildconsole.ulti.io/dashboard/64e62b8a1aa339d31db3edf1/builds?page=0&pagesize=20"
     driver.get(url)
     pipeline = WebDriverWait(driver, 20).until(visibility_of_element_located((By.XPATH, Util.Pipeline_Name_XPATH))).text
 
@@ -20,14 +20,16 @@ def getBuildConsoleUrls():
         print("P0s Pipeline")
         sleep(10)
 
-        for i in range(1, 21):
-            build_columns = driver.find_elements(By.XPATH, Util.Builds_Table_Row_XPATH.replace("(iterator)", str(i)))
-
+        for i in range(1, 20):
+            sleep(0.3)
             build_selector = Util.Builds_Table_Row_XPATH.replace("(iterator)", str(i)) + "//td"
 
-            build_number = driver.find_element(By.XPATH, build_selector + "//a[@style='text-decoration: unset; color: #3f51b5;']").text
+            build_number = driver.find_element(By.XPATH, Util.Build_Number_XPATH.replace("(iterator)", str(i + 1))).text
+            build_date = driver.find_element(By.XPATH, Util.Build_Date_XPATH.replace("(iterator)", str(i + 1))).text
             
             try:
+                ActionChains(driver).send_keys(Keys.ESCAPE).perform()
+
                 driver.find_element(By.XPATH, build_selector + "//span[@ng-reflect-ng-class='failure-text']").click()
                 driver.find_element(By.XPATH, Util.Run_Tests_Button_XPATH).click()
 
@@ -39,7 +41,7 @@ def getBuildConsoleUrls():
                     build_id = tc_url.split("buildId=")[1].split("&buildTypeId=")[0]
                     url = f"https://teamcity.dev.us.corp/buildConfiguration/UltiPro_Dev5Quality_4Integration_1Domains_Gate9_00RunTests/{build_id}?buildTab=tests&status=failed"
                     
-                    urls[build_number] = url
+                    urls[build_number + "/*/" + build_date] = url
                 except Exception:
                     build_id = "[!]"
 
