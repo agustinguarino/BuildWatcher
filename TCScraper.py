7cc63909d18f298375a956d0ede2886e0b2b9541
Agustin Guarino <agustinignacio.guarinobonahon@ukg.com>
2023-11-06 12:51:18 -0300

    Replaced hardcoded elements with references to Util file, and added new way to detect test package

--- TCScraper.py
+++ TCScraper.py
@@ -1,54 +1,60 @@
 from selenium import webdriver
 from selenium.webdriver.common.by import By
+from selenium.webdriver.support.wait import WebDriverWait
+from selenium.webdriver.support.expected_conditions import visibility_of_element_located
 from time import sleep
 import Util as Util
 
-urls = ["https://teamcity.dev.us.corp/buildConfiguration/UltiPro_Dev5Quality_4Integration_1Domains_Gate9_00RunTests/52499069?buildTab=tests&status=failed"]
+urls = ["https://teamcity.dev.us.corp/buildConfiguration/UltiPro_Dev5Quality_4Integration_1Domains_Gate9_00RunTests/52530534?buildTab=tests&status=failed"]
+#urls = ["https://teamcity.dev.us.corp/buildConfiguration/UltiPro_Dev5Quality_4Integration_1Domains_Gate9_00RunTests/52739671?buildTab=tests&status=failed"]
 driver = webdriver.Chrome()
 failure_elements = []
 
 for url in urls:
-    driver.get(url)
+    driver.get("https://teamcity.dev.us.corp/favorite/projects")
 
     ## Automate process until get to password and PingID code needed
 
-    print("Sleeping for 45 secs")
-    sleep(45)
-    driver.get(url)
-    print("After sleeping")
+    try:
+        login_locator = driver.find_element(By.XPATH, Util.Login_Page_Indicator_XPATH)
+        login_locator.click()
+        WebDriverWait(driver, 120).until(visibility_of_element_located((By.XPATH, Util.HomePage_Indicator_XPATH)))
+    except Exception:
+        pass
 
     driver.get(url)
 
-    print("Starting scraping")
     sleep(5)
     failure_elements = driver.find_elements(By.XPATH, "//div[@class='TestItem__expandable--KK TestItemAdvanced__row--pF']") # Util.Failure_Elements_XPATH
     print(f"Errors detected: {str(len(failure_elements))}")
-    print("Ending scraping")
 
 
 # For each error element found in TeamCity
 for i in range (0, len(failure_elements)):
     print("Arrive")
-    arrow_down = driver.find_element(By.XPATH, f"(//div[@class='Details__heading--id TestItem__heading--Xx TestItem__expandable--KK']/span[@class='ring-icon-icon SvgIcon__icon--wZ TestItem__arrow--TC'])[{str(i + 1)}]")
+    arrow_down = driver.find_element(By.XPATH, Util.Arrow_Down_XPATH.replace("(iterator)", str(i + 1)))
     driver.execute_script("arguments[0].click();", arrow_down)
     sleep(1)
 
     # Get test name
-    test_name = driver.find_element(By.XPATH, f"(//a[@data-test-build-test-name-part='name'])[{str(i + 1)}]").text
+    test_name = driver.find_element(By.XPATH, Util.Test_Name_XPATH.replace("(iterator)", str(i + 1))).text
     
     # Get package name if present
     try:
-        package = driver.find_element(By.XPATH, f"(//a[@data-test-build-test-name-part='package'])[{str(i + 1)}]").text
+        package = driver.find_element(By.XPATH, Util.Package_Name_XPATH.replace("(iterator)", str(i + 1))).text
     except Exception:
-        package = "No package found."
+        try:
+            package = driver.find_element(By.XPATH, Util.Secondary_Package_XPATH.replace("(iterator)", str(i + 1))).text
+        except Exception:
+            package = "No package found."
 
     # Get flaky test inficator if present
     try:
-        flaky_test_indicator = driver.find_element(By.XPATH, Util.Flaky_Test_Indicator_XPATH).text
-        if flaky_test_indicator == "flaky": flaky_test = True
+        flaky_test_indicator = driver.find_element(By.XPATH, Util.Flaky_Test_Indicator_XPATH.replace("(iterator)", str(i + 1))).text
+        if flaky_test_indicator == "flaky": flaky_test = "True"
         
     except Exception:
-        flaky_test = False
+        flaky_test = "False"
 
     # Get test duration if present
     try:
@@ -64,6 +70,6 @@ for i in range (0, len(failure_elements)):
 
     ## Make package optional (some tests dont have the element)
     ## Check for flaky tag
-    print(f"{test_name}")
+    print(f"{test_name} // {package} // {flaky_test} // {duration} // {stacktrace[:200]}")
     sleep(0.5)
     
